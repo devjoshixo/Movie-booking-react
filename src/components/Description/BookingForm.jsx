8e3ca8c2d326e835c3474fb6a0cbfa228077a443
devjoshixo <djoshi923@gmail.com>
2023-08-08 04:33:21 +0530

    Form more

--- src/components/Description/BookingForm.jsx
+++ src/components/Description/BookingForm.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './BookingForm.module.css';
 
 const BookingForm = (props) => {
+  const [dateAvailable, setDateAvailable] = useState(null);
+  const [formData, setFormData] = useState({
+    name: '',
+  });
+
+  useEffect(() => {
+    const date = new Date();
+    let currentDay = String(date.getDate()).padStart(2, '0');
+    let currentMonth = String(date.getMonth() + 1).padStart(2, '0');
+    let currentYear = date.getFullYear();
+    let currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+    setDateAvailable(currentDate);
+  }, []);
+
   return (
     <div className={classes.outer}>
       <div className={classes.bookingform}>
@@ -11,13 +25,17 @@ const BookingForm = (props) => {
             style={{ color: '#ffffff' }}
             onClick={props.onOpenForm}
           ></i>
-          <div className={classes.name}>
-            <h3>Name</h3>
+          <div className={`${classes.name} ${classes.inputs}`}>
+            <h3 className={classes.heading}>Name</h3>
             <input type='text' placeholder='Enter your name' />
           </div>
-          <div className={classes.seat}>
-            <h3>Seats</h3>
-            <input type='text' placeholder='Enter number of seats' />
+          <div className={`${classes.seat} ${classes.inputs}`}>
+            <h3 className={classes.heading}>Seats</h3>
+            <input type='number' placeholder='Enter number of seats' />
+          </div>
+          <div className={`${classes.showdate} ${classes.inputs}`}>
+            <h3 className={classes.heading}>Show Date</h3>
+            <input type='date' min={dateAvailable} />
           </div>
         </form>
       </div>
